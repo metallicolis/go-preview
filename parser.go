e92bcb72c547dbb6415277b10dfad7cccd7c5d2f
Alex <metallicolis@gmail.com>
2019-10-17 22:51:46 -0500

    test

--- parser.go
+++ parser.go
@@ -1,5 +1,28 @@
 package parser
 
-func Hello() string {
-	return "Hello"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+func parse(url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := html.Parse(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf(body.Data)
+
+	return "body"
 }
